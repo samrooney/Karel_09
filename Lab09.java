619c011611220d964818092779758ff9921aedf7
samrooney <sam_rooney@asl.org>
2014-12-07 13:00:34 +0000

    finished

--- Lab09.java
+++ Lab09.java
@@ -7,17 +7,53 @@
  *
  */
 
-import edu.fcps.karel2.Display; 
-import javax.swing.JOptionPane;
+
+      import edu.fcps.karel2.Display;
+      import edu.fcps.karel2.Robot;  
+      import javax.swing.JOptionPane;
 
 public class Lab09 {
 	 
 	 public static void main(String[] args) {
-		 String map = JOptionPane.showInputDialog("Which map?");
-		 Display.openWorld("maps/"+map+".map");
+		 
+       String map = JOptionPane.showInputDialog("Which map?");
+		 Display.openWorld("maps/" + map + ".map");
 		 Display.setSize(10, 10);
-		 Athlete athena = new Athlete(1, 1, Display.EAST, 0);
-		 //TODO write a combination of definite and indefinite loops that will cause athena
-		 // to pick up each pile of beepers and deposit them one square to the right.
-	 }
- }
+		 Display.setSpeed(10);
+       
+       Athlete Dooby = new Athlete(1, 1, Display.EAST, 0);
+		 
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       
+       for (int i = 1; i <= 7; i++) {
+       int count = 0;
+       while (Dooby.nextToABeeper())
+      {
+       Dooby.pickBeeper();
+       count++;
+      }
+       Dooby.move();
+       for (int k = 1; k <= count; count--)
+      {
+       Dooby.putBeeper();
+      }
+      Dooby.turnAround();
+       if (Dooby.frontIsClear()) 
+      {
+       Dooby.move();
+      }
+       if (Dooby.frontIsClear()) 
+      {
+       Dooby.move();
+      }
+       Dooby.turnAround();
+     }
+       Dooby.turnAround();
+     
+  }
+}
